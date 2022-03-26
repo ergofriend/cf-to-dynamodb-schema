90b2633f7482d102d2d67cbd38ffc0d967fafa76
ErgoFriend <erg.friend@gmail.com>
2022-03-26 16:48:49 +0900

    add parse func

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import {execSync} from 'child_process'
 
+const parse = (data: string) => {
+  try {
+    return JSON.parse(data)
+  } catch (error) {
+    throw Error('failed parse template')
+  }
+}
+
 export const parseTemplate = (data: string): string => {
-  const template = JSON.parse(data)
+  const template = parse(data)
   const resources = template.Resources
   if (!resources) throw Error('not found Resources')
   const resourcesValues = Object.values(resources)
